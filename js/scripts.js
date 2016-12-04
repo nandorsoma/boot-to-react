a5daace3b0cf7a6432388a0989d89b1489bb1120
Nándor Soma Abonyi <abonyisoma@gmail.com>
2016-12-04 21:18:11 +0100

    gitignore and little refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-var _ = require('underscore');
+//var _ = require('underscore');
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component{
-  render(){
-    return(
-      <h1>It works!</h1>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 
